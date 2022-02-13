a20630c468e30f71e5e8f6234e96aba0d5b7d78e
Raisess <danilosantana456789@gmail.com>
2022-02-13 15:13:18 -0300

    feat(Platform): set green color by default

--- game/src/Game/Platform.cpp
+++ game/src/Game/Platform.cpp
@@ -2,6 +2,7 @@
 
 Game::Platform::Platform(Engine::Managers::ScreenComponentManager* component_manager, PlatformOptions platform_options) {
   this->component = component_manager->create_component(platform_options.x, platform_options.y, platform_options.width, platform_options.height, platform_options.fill);
+  this->component->set_color(0, 255, 0);
 }
 
 std::vector<Game::Platform*> Game::Platform::create_many(Engine::Managers::ScreenComponentManager* component_manager, std::vector<PlatformOptions> platforms_options) {
