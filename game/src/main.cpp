11078f4b07c6e8cb844c5011ad14eaa0ef720443
Raisess <danilosantana456789@gmail.com>
2022-02-13 15:51:34 -0300

    chore: improve main loop

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "Engine/Window.h"
 #include "Engine/Camera.h"
 #include "Engine/Managers/ScreenComponentManager.h"
@@ -17,7 +18,8 @@ int main(int argc, char** argv) {
   window->set_backgroud_color(0, 0, 255);
 
   auto window_size = window->get_size();
-  Size world_size = {
+  const int middle_screen = window_size.width / 2;
+  const Size world_size = {
     1500,
     1000,
   };
@@ -25,40 +27,42 @@ int main(int argc, char** argv) {
   auto* text_component_manager = new Engine::Managers::TextComponentManager(window);
   auto* screen_component_manager = new Engine::Managers::ScreenComponentManager(window);
 
-  const int middle_screen = window_size.width / 2;
-
   auto* fps_text = text_component_manager->create_component(0, 0, 100, 30, 24);
   fps_text->set_color(255, 255, 0);
-  auto* test_text = text_component_manager->create_component(middle_screen - 50, 0, 100, 30, 24);
-  test_text->set_color(255, 255, 255);
-  test_text->set_value("Test Text");
 
   auto* player = new Game::Player(screen_component_manager, { middle_screen + 500, 0, 50, 50, FILL_PLAYER });
+  auto* enemy = new Game::Player(screen_component_manager, { middle_screen + 600, 0, 50, 50, FILL_PLAYER });
+  std::vector<Game::Player*> live_entities = { player, enemy };
+
   auto platforms = Game::Platform::create_many(screen_component_manager, {
     { middle_screen, 440, 1000, 40, FILL_FLOOR },
     { middle_screen + 440, 300, 200, 40, FILL_FLOOR },
     { middle_screen + 250, 150, 200, 40, FILL_FLOOR },
   });
+
+  enemy->component->set_color(255, 255, 0);
   
   const auto callback = [&]() -> void {
     Engine::Camera::set_camera_viewport(window, world_size, player->component);
 
-    auto player_pos = player->component->get_position();
-    auto player_size = player->component->get_size();
-
     player->detect_keydown();
-    player->apply_gravity();
 
-    auto new_player_pos = player->component->get_position();
-    test_text->set_value("Is not colliding");
+    for (auto entity : live_entities) {
+      entity->apply_gravity();
+    }
+
     for (auto platform : platforms) {
       auto platform_pos = platform->component->get_position();
       auto platform_size = platform->component->get_size();
 
-      if (player->is_colliding(platform->component)) {
-        player->component->set_gravity_speed(0);
-        player->component->set_position(new_player_pos.x, platform_pos.y - platform_size.height - (player_size.height - platform_size.height));
-        test_text->set_value("Is colliding");
+      for (auto entity : live_entities) {
+        auto entity_pos = entity->component->get_position();
+        auto entity_size = entity->component->get_size();
+
+        if (entity->is_colliding(platform->component)) {
+          entity->component->set_gravity_speed(0);
+          entity->component->set_position(entity_pos.x, platform_pos.y - platform_size.height - (entity_size.height - platform_size.height));
+        }
       }
     }
 
